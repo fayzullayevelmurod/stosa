ee5cc2a2b3aa132db93f53c6ed4feaf1f1c137d1
SardorQobilov <note47297@gmail.com>
2024-09-05 12:03:29 +0500

    home page desktop version finished

--- js/main.js
+++ js/main.js
@@ -17,24 +17,51 @@ window.addEventListener('DOMContentLoaded', () => {
 
   whyStosaCard.forEach(item => {
     const plusBtn = item.querySelector('.plus-btn');
-  
+
     plusBtn.addEventListener('click', () => {
       // Faqat o'zining kartochkasiga active klassini qo'shish yoki olib tashlash
       const isActive = item.classList.contains('active');
-  
+
       // Barcha kartochkalardagi active klassini o'chirish
-      whyStosaCard.forEach(el => el.classList.remove('active'));
-  
+      whyStosaCard.forEach(el => {
+        el.classList.remove('active');
+        plusBtn.classList.remove('active');
+      });
+
       // Agar o'zining kartochkasi aktiv bo'lmasa, uni aktiv qilish
       if (!isActive) {
         item.classList.add('active');
+        plusBtn.classList.add('active');
       }
-      
-      // Plus tugmasiga active klassini qo'shish yoki olib tashlash
-      plusBtn.classList.toggle('active');
     });
   });
-  
 
+
+  // news-events__swiper
+  // Swiperni initsializatsiya qilish
+  const swiper = new Swiper('.news-events__swiper', {
+    slidesPerView: 3.5, // Bir vaqtda ko'rsatiladigan slaydlar soni
+    spaceBetween: 40, // Slaydlar o'rtasidagi masofa (px)
+    navigation: {
+      nextEl: '.news-events__swiper-next',
+      prevEl: '.news-events__swiper-prev',
+    },
+    speed: 700,
+    // Qo'shimcha sozlamalar (masalan, responsive)
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
 
 })
